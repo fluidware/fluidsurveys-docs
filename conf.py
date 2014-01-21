3820dd353e1cd4b7d6236246ae995c02f2dd4ae4
Samuel Cormier-Iijima <sciyoshi@gmail.com>
2014-01-21 11:52:11 -0500

    Fix theme path for server compilation

--- conf.py
+++ conf.py
@@ -96,7 +96,7 @@ pygments_style = 'sphinx'
 
 # Activate the theme.
 html_theme = 'theme'
-html_theme_path = '../theme'
+html_theme_path = ['.']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
